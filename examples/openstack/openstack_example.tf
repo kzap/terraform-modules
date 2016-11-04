7ad31dcc1286eca223be74fe98f812ae68d5b07c
Andre Marcelo-Tanner <andre@enthropia.com>
2016-11-04 09:52:49 +0800

    - remove _app from variable names in example

--- examples/openstack/openstack_example.tf
+++ examples/openstack/openstack_example.tf
@@ -5,15 +5,15 @@ module "openstack_app" {
     prefix = "${var.env}-app"
     public_key = "${file("${var.public_key_file}")}"
     key_file_path = "${var.private_key_file}"
-    servers = "${var.openstack_app_servers}"
+    servers = "${var.openstack_servers}"
     
     # OpenStack config
     username = "${var.openstack_username}"
     tenant_name = "${var.openstack_tenant_name}"
     password = "${var.openstack_password}"
     region = "${var.openstack_region}"
-    image_id = "${var.openstack_app_image}"
-    flavor_id = "${var.openstack_app_flavor}"
+    image_id = "${var.openstack_image}"
+    flavor_id = "${var.openstack_flavor}"
 
     # OpenStack defaults
     auth_url = "${var.openstack_auth_url}"
@@ -25,7 +25,7 @@ module "centos_provisioner" {
     source = "github.com/kzap/tf-lamp//provisioners/bash/centos-7/app-db-server"
     
     # Server Info
-    servers = "${var.openstack_app_servers}"
+    servers = "${var.openstack_servers}"
     server_ips = ["${module.openstack_app.nodes_floating_ips}"]
 
     # Login Information
